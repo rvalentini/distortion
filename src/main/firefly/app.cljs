51deaba1742e8538b033b1d931e6047d063d4e47
rvalentini <ri.valentini@reply.de>
2022-04-29 18:56:49 +0200

    WIP Add diamond pattern background as "test object" for distortion

--- src/main/firefly/app.cljs
+++ src/main/firefly/app.cljs
@@ -1,35 +1,30 @@
 (ns firefly.app
   (:require [reagent.core :as r]
-            [reagent.dom :as rdom]                          ;TODO understand rdom vs. core ns.
+            [reagent.dom :as rdom]
             [quil.core :as q]
             [quil.middleware :as m]))
+
+(defn diamond-background []
+  (let [steps (range 0 510 10)
+        points-along-x (map #(identity [0 %]) steps)
+        points-along-y (map #(identity [% 0]) steps)]
+    (q/stroke 50 0 0)
+    (q/stroke-weight 2)
+    (doseq [[p1 p2] (map vector points-along-x points-along-y)]
+      (q/line p1 p2))))
 
 (defn setup []
   (q/smooth)
   (q/background 230 230 230)
-  (q/stroke 130, 0 0)
-  (q/stroke-weight 4)
-  (let [cross-size      70
-        circ-size       50
-        canvas-x-center (/ (q/width) 2)
-        canvas-y-center (/ (q/height) 2)
-        left            (- canvas-x-center cross-size)
-        right           (+ canvas-x-center cross-size)
-        top             (+ canvas-y-center cross-size)
-        bottom          (- canvas-y-center cross-size)]
-    (q/line left bottom right top)
-    (q/line right bottom left top)
-
-    (q/fill 255 150)
-    (q/ellipse canvas-x-center canvas-y-center circ-size circ-size)))
+  (diamond-background))
 
 (defn canvas []
   (r/create-class
     {:component-did-mount
      (fn [component]
-       (let [node (rdom/dom-node component)                 ;TODO understand what is dom-node doing?
-             width (/ (.-innerWidth js/window) 2)
-             height (/ (.-innerHeight js/window) 2)]
+       (let [node (rdom/dom-node component)
+             width 500
+             height 500]
          (q/sketch
            :title "Red cross"
            :host node
@@ -37,7 +32,7 @@
            :size [width height]
            :middleware [m/fun-mode])))
      :render
-     (fn [] [:div])})) ;TODO why :div here?
+     (fn [] [:div])}))
 
 
 (defn ^:dev/after-load run []
