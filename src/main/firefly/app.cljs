6c52a86d0b25108d699f37ef82c84e13d7250ba9
rvalentini <ri.valentini@reply.de>
2022-05-08 17:31:53 +0200

    Implement a basic point based distortion

--- src/main/firefly/app.cljs
+++ src/main/firefly/app.cljs
@@ -10,24 +10,50 @@
 
 ; TODO use width and height instead
 ; TODO idea: color blocks by finding closest square limits surrounding a given point
+; TODO idea: generify distortion by passing dist-fn
 
-(defn diamond-background []
-  (let [steps (range 0 510 20)
-        points-along-x (mapcat (fn [s] [[0 s] [width s]]) steps)
-        points-along-y (mapcat (fn [s] [[s 0] [s height]]) steps)]
+(defn distort [[x y] factor [x-center y-center]]
+  (q/with-stroke ["green"]
+    (q/stroke-weight 5)
+    (q/point x-center y-center))
+  (q/stroke-weight 2)
+  (let [dist-u-x (- x x-center)
+        dist-u-y (- y y-center)
+        dist-u-x-norm (/ (- dist-u-x width) width)
+        dist-u-y-norm (/ (- dist-u-y height) height)
+        dist-d-x (* dist-u-x-norm (- 1 (* factor (Math/pow dist-u-x-norm 2))))
+        dist-d-y (* dist-u-y-norm (- 1 (* factor (Math/pow dist-u-y-norm 2))))
+        x-d (* dist-d-x x)
+        y-d (* dist-d-y y)]
+    [x-d y-d]))
+
+(defn diamonds []
+  (let [steps (range 0 50)
+        points (for [x steps y steps] [x y])
+        nodes (filter (fn [p] (not (or (every? odd? p) (every? even? p)))) points)
+        scaled (map (fn [p] (mapv #(* 10 %) p)) nodes)]
     (q/stroke blackish)
     (q/stroke-weight 2)
-    (doseq [[p1 p2] (map vector points-along-x points-along-y)]
-      (q/line p1 p2)
-      (q/with-rotation [(- q/HALF-PI)]
-        (q/with-translation [(- height) 0]
-          (q/line p1 p2))))))
+    (doseq [[x y] scaled]
+      (q/with-stroke ["blue"]
+        (q/line [x y] [(+ 10 x) (+ 10 y)])
+        (q/line [x y] [(- x 10) (+ 10 y)])))
+    (doseq [[x y] scaled]
+      (q/line (distort [x y] 0.1 [250 250]) (distort [(+ 10 x) (+ 10 y)] 0.1 [250 250]))
+      (q/line (distort [x y] 0.1 [250 250]) (distort [(- x 10) (+ 10 y)] 0.1 [250 250])))))
+
+
+(defn debug-mark-origin []
+  (q/stroke "red")
+  (q/stroke-weight 5)
+  (q/point 0 0))
 
 (defn setup []
   (q/smooth)
   (q/background 230 230 230)
   (q/with-translation [250 250]
-    (diamond-background)))
+    (debug-mark-origin)
+    (diamonds)))
 
 (defn canvas []
   (r/create-class
@@ -54,5 +80,6 @@
   (shadow/repl :app)
   (js/alert "Bonjour from REPL")
 
-  (println {:a 444 :b 88})
-  )
+  (def p1 [270 270])
+  (distort [1 1] 0.1 [250 250])
+  (println {:a 444 :b 88}))
