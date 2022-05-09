9f38d395f3ed704dd7abc5adb3a233045ec6cdd8
rvalentini <ri.valentini@reply.de>
2022-05-09 21:41:08 +0200

    Implement two distortion variants: barrel-like and pincushion-like

--- src/main/firefly/app.cljs
+++ src/main/firefly/app.cljs
@@ -9,39 +9,49 @@
 (def blackish [50 0 0])
 
 ; TODO use width and height instead
+; TODO make it move
 ; TODO idea: color blocks by finding closest square limits surrounding a given point
-; TODO idea: generify distortion by passing dist-fn
+; TODO idea: combine multiple distortions in sequence
 
-(defn distort [[x y] factor [x-center y-center]]
-  (q/with-stroke ["green"]
-    (q/stroke-weight 5)
-    (q/point x-center y-center))
-  (q/stroke-weight 2)
-  (let [dist-u-x (- x x-center)
-        dist-u-y (- y y-center)
-        dist-u-x-norm (/ (- dist-u-x width) width)
-        dist-u-y-norm (/ (- dist-u-y height) height)
-        dist-d-x (* dist-u-x-norm (- 1 (* factor (Math/pow dist-u-x-norm 2))))
-        dist-d-y (* dist-u-y-norm (- 1 (* factor (Math/pow dist-u-y-norm 2))))
-        x-d (* dist-d-x x)
-        y-d (* dist-d-y y)]
-    [x-d y-d]))
+(defn line [x y]
+  ;TODO conditional draw when on canvas
+  [x y])
+
+(defn barrel-like-dist [undistorted]
+  (+ undistorted (* 2 (Math/pow (Math/log undistorted) 2))))
+
+(defn pincushion-like-dist [undistorted]
+  (+ undistorted (/ (Math/pow undistorted 2) 300)))
+
+(defn distort [[x y] fn [center-x center-y]]
+  (let [[v-x v-y] [(- x center-x) (- y center-y)]
+        v-len (Math/sqrt (+ (Math/pow v-x 2) (Math/pow v-y 2)))
+        [v-norm-x v-norm-y] [(/ v-x v-len) (/ v-y v-len)]
+        distorted (fn v-len)
+        [new-v-x new-v-y] [(* distorted v-norm-x) (* distorted v-norm-y)]
+        new-p [(+ new-v-x center-x) (+ new-v-y center-y)]]
+    new-p))
 
 (defn diamonds []
-  (let [steps (range 0 50)
+  (let [steps (range 0 50)                                  ;TODO increase
         points (for [x steps y steps] [x y])
         nodes (filter (fn [p] (not (or (every? odd? p) (every? even? p)))) points)
-        scaled (map (fn [p] (mapv #(* 10 %) p)) nodes)]
+        scaled (map (fn [p] (mapv #(* 10 %) p)) nodes)
+        distort-fn barrel-like-dist]
     (q/stroke blackish)
+    (q/stroke-weight 2)
+    (q/with-stroke ["green"]
+      (q/stroke-weight 5)
+      (q/point 250 250))
     (q/stroke-weight 2)
     (doseq [[x y] scaled]
       (q/with-stroke ["blue"]
-        (q/line [x y] [(+ 10 x) (+ 10 y)])
-        (q/line [x y] [(- x 10) (+ 10 y)])))
-    (doseq [[x y] scaled]
-      (q/line (distort [x y] 0.1 [250 250]) (distort [(+ 10 x) (+ 10 y)] 0.1 [250 250]))
-      (q/line (distort [x y] 0.1 [250 250]) (distort [(- x 10) (+ 10 y)] 0.1 [250 250])))))
-
+        (q/line
+          (distort [x y] distort-fn [250 250])
+          (distort [(+ 10 x) (+ 10 y)] distort-fn [250 250]))
+        (q/line
+          (distort [x y] distort-fn [250 250])
+          (distort [(- x 10) (+ 10 y)] distort-fn [250 250]))))))
 
 (defn debug-mark-origin []
   (q/stroke "red")
@@ -79,6 +89,8 @@
   ;; switch to CLJS REPL
   (shadow/repl :app)
   (js/alert "Bonjour from REPL")
+
+  (distort [400 400] 0.1 [250 250])
 
   (def p1 [270 270])
   (distort [1 1] 0.1 [250 250])
