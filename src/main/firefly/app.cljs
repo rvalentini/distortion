fd43e5d947a9a9e7101038db831f34b8f6e012ef
rvalentini <ri.valentini@reply.de>
2022-05-16 21:24:26 +0200

    Implement colored diamonds

--- src/main/firefly/app.cljs
+++ src/main/firefly/app.cljs
@@ -13,10 +13,6 @@
 ; TODO idea: color blocks by finding closest square limits surrounding a given point
 ; TODO idea: combine multiple distortions in sequence
 
-(defn line [x y]
-  ;TODO conditional draw when on canvas
-  [x y])
-
 (defn barrel-like-dist [undistorted]
   (+ undistorted (* 2 (Math/pow (Math/log undistorted) 2))))
 
@@ -32,26 +28,56 @@
         new-p [(+ new-v-x center-x) (+ new-v-y center-y)]]
     new-p))
 
+(defn within-bounds [location dim]
+  (every? #(< -1 % dim) location))
+
+(defn get-neighbors [[x y] points]
+  (map #(get-in points %) [[x (dec y)] [(dec x) y] [(inc x) y] [x (inc y)]]))
+
+(defn draw-quad [[[t1 t2] [r1 r2] [b1 b2] [l1 l2]]]
+  (q/quad t1 t2 r1 r2 b1 b2 l1 l2))
+
+(defn draw-diamond [[t l r _ :as diamond]]
+  (println diamond)                                         ;TODO fix case all nil should not happen
+  (when (some? t)
+    (when (some? l)
+      (q/with-stroke ["blue"]
+        (q/line t l)))
+    (when (some? r)
+      (q/with-stroke ["blue"]
+        (q/line t r)))
+    (when (every? some? diamond)
+      (q/with-fill ["yellow"] (draw-quad diamond)))))
+
+(defn filter-center-points [points]
+  (mapv
+    (fn [[idx row]] (vec (take-nth 2 (if (even? idx) row (rest row)))))
+    (map-indexed vector points)))
+
+; (1) generate matrix as vec of vecs
+; (2) distort points
+; (3) get uneven points in uneven rows and even in even
+; (4) map to get-neighbors for these points
+; (5) apply draw logic depending if [T L R B] exist + color coding depending on distance
+
 (defn diamonds []
+  ;TODO use threading
   (let [steps (range 0 50)                                  ;TODO increase
-        points (for [x steps y steps] [x y])
-        nodes (filter (fn [p] (not (or (every? odd? p) (every? even? p)))) points)
-        scaled (map (fn [p] (mapv #(* 10 %) p)) nodes)
-        distort-fn barrel-like-dist]
+        points (vec (for [x steps]
+                      (vec (for [y steps] [(* 10 x) (* 10 y)]))))
+        distorted (map #(map (fn [p] (distort p barrel-like-dist [250 250])) %) points)
+        center-points (filter-center-points distorted)
+        diamonds (for [i (range 0 50)
+                       j (range 0 50)]
+                   (get-neighbors [i j] center-points))]
     (q/stroke blackish)
     (q/stroke-weight 2)
     (q/with-stroke ["green"]
       (q/stroke-weight 5)
       (q/point 250 250))
     (q/stroke-weight 2)
-    (doseq [[x y] scaled]
-      (q/with-stroke ["blue"]
-        (q/line
-          (distort [x y] distort-fn [250 250])
-          (distort [(+ 10 x) (+ 10 y)] distort-fn [250 250]))
-        (q/line
-          (distort [x y] distort-fn [250 250])
-          (distort [(- x 10) (+ 10 y)] distort-fn [250 250]))))))
+    (doseq [d diamonds]
+      (draw-diamond d))))
 
 (defn debug-mark-origin []
   (q/stroke "red")
@@ -90,7 +116,12 @@
   (shadow/repl :app)
   (js/alert "Bonjour from REPL")
 
-  (distort [400 400] 0.1 [250 250])
+  (def test-vec (vec (for [i (range 0 5)]
+                       (vec (for [j (range 0 5)] [i j])))))
+
+  test-vec
+  (mapv (fn [[idx row]] (vec (take-nth 2 (if (even? idx) row (rest row))))) (map-indexed vector test-vec))
+
 
   (def p1 [270 270])
   (distort [1 1] 0.1 [250 250])
